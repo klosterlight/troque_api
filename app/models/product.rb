e9e5a6b2e0852fcf61a856cedc0b850af4d25216
klosterlight <gerry.rendon@gmail.com>
2022-11-14 19:57:21 -0600

    Product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates_presence_of :name
+
+  enum status: [:pending, :published]
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    FactoryBot.build(:product)
+  }
+
+  describe 'associations' do
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  it do
+    should define_enum_for(:status).
+      with_values([:pending, :published])
+  end
 end
